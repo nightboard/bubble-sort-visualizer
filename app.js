23d59315326f94c17ce1f66404ad8d5b86582e76
visrut <visrutnavadiya111@gmail.com>
2021-09-09 20:54:31 +0530

    complete buggy code

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 class Bubbles {
-  constructor(count, max_radius = 100) {
+  constructor(count, max_radius = 100, delay = 1000) {
     this.count = count;
     this.max_radius = max_radius;
+    this.delay = delay;
   }
 
   getRandomRadius() {
     let radius = Math.floor(Math.random() * this.max_radius + 1);
     if (radius <= 20) {
-      radius += 10;
+      radius += 20;
+    }
+    if (radius >= this.max_radius - 10) {
+      radius -= 20;
     }
     if (radius >= this.max_radius - 20) {
-      radius -= 20;
+      radius -= 10;
+    }
+    if (radius >= this.max_radius - 30) {
+      radius -= 10;
     }
     return radius;
   }
@@ -30,6 +37,7 @@ class Bubbles {
     const container = document.querySelector(selector);
     for (let i = 0; i < this.count; i++) {
       const bubble = this.getBubble(this.max_radius);
+      bubble.style.top = `${i * 50}px`;
       container.appendChild(bubble);
     }
   }
@@ -38,22 +46,54 @@ class Bubbles {
     return parseInt(node.textContent);
   }
 
-  bubbleSort(selector) {
+  delayTime(time) {
+    return new Promise((work) => {
+      setTimeout(() => {
+        work();
+      }, time);
+    });
+  }
+
+  async makeBubbleActive(bubble1, bubble2) {
+    await this.delayTime(this.delay);
+    bubble1.classList.add("active");
+    await this.delayTime(this.delay);
+    bubble2.classList.add("active");
+  }
+
+  async makeBubbleNormal(bubble1, bubble2) {
+    await this.delayTime(this.delay);
+    bubble1.classList.remove("active");
+    await this.delayTime(this.delay);
+    bubble2.classList.remove("active");
+  }
+
+  async swapBubbles(bubble1, bubble2) {
+    const bubble1Top = bubble1.style.top;
+    const bubble2Top = bubble2.style.top;
+    await this.makeBubbleActive(bubble1, bubble2);
+    await this.delayTime(this.delay);
+    // swap property
+    bubble1.style.top = bubble2Top;
+    bubble2.style.top = bubble1Top;
+    await this.makeBubbleNormal(bubble1, bubble2);
+  }
+
+  async bubbleSort(selector) {
     const nodes = document.querySelector(selector).children;
     let isSwapped = false;
-    for (let i = 0; i < nodes.length; i++) {
-      for (let j = i; j <= nodes.length - i - 1; j++) {
-        if (this.getBubbleValue(nodes[j]) > this.getBubbleValue(nodes[i])) {
-          isSwapped = true;
-          const array = [...nodes];
-          console.log(array);
 
-          // swap elements
-          const temp = array[j];
-          array[j] = array[i];
-          array[i] = temp;
+    for (let i = 0; i < this.count; i++) {
+      for (let j = 0; j < this.count - i - 1; j++) {
+        if (this.getBubbleValue(nodes[j]) > this.getBubbleValue(nodes[j + 1])) {
+          isSwapped = true;
+          // swap elements | swap top property of bubble
+          this.swapBubbles(nodes[j], nodes[j + 1]);
+          await this.delayTime(this.delay);
         }
       }
+
+      console.log(nodes);
 
       if (isSwapped == false) {
         break;
